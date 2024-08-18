18319bc05d176a0cbd6a8d6bf9df3e161a943e4f
Victor Ribeiro <devjvribeiro@gmail.com>
2024-08-18 00:13:01 -0300

    Remove unnecessary paragraph from home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <p>Click the button below to navigate to the about page</p>
     <NuxtLink to="/nested/1/nested">Nested Page</NuxtLink>
   </div>
 </template>
